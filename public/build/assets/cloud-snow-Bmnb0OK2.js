2bdc60a177e3503f02051acf463caaafe078e7d9
joe-dev <joechristianbayucan252@gmail.com>
2026-09-07 16:54:47 +0800

    build: Add compiled assets for disaster icons and weather improvements
    - Updated DisasterManagement bundle with dynamic disaster icons (67.39 kB)
    - Updated WeatherDashboard bundle with accurate weather icons (39.26 kB)
    - Updated Dashboard, CasualtyGraph, InjuredGraph, MissingGraph bundles
    - Added new cloud-snow icon asset
    - Updated CSS bundle (120.57 kB) with new icon styles
    - Updated manifest.json with new asset hashes
    - All assets properly optimized and gzipped

---
+++ public/build/assets/cloud-snow-Bmnb0OK2.js
@@ -0,0 +1,11 @@
+import{c as t}from"./plus-ColiOj16.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M6 16.326A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 .5 8.973",key:"1cez44"}],["path",{d:"m13 12-3 5h4l-3 5",key:"1t22er"}]],d=t("cloud-lightning",h);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242",key:"1pljnt"}],["path",{d:"M8 15h.01",key:"a7atzg"}],["path",{d:"M8 19h.01",key:"puxtts"}],["path",{d:"M12 17h.01",key:"p32p05"}],["path",{d:"M12 21h.01",key:"h35vbk"}],["path",{d:"M16 15h.01",key:"rnfrdf"}],["path",{d:"M16 19h.01",key:"1vcnzz"}]],e=t("cloud-snow",a);export{d as C,e as a};
